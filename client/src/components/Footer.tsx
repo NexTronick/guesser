a1f655f9f8f32fa62a5905dafa52da8f08ed9826
Natraj Padwani <nextronickk@gmail.com>
2024-04-08 05:09:11 +0800

    Updated package.json, added start and build functionallity with npm

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,12 +1,4 @@
-import {
-  Box,
-  Container,
-  Paper,
-  Typography,
-  BottomNavigation,
-} from "@mui/material";
 import React from "react";
-import { ThemeType } from "../AllTypes";
 import { useAppSelector } from "../app/hooks";
 import { selectTheme } from "../features/theme/themeSlice";
 
@@ -14,50 +6,6 @@ export default function Footer() {
   const theme = useAppSelector(selectTheme);
   return (
     <div className="bottom-0 absolute w-full">
-      {/* <Paper
-        sx={{
-          marginTop: "calc(10% + 60px)",
-          width: "100%",
-          position: "fixed",
-          bottom: 0,
-        }}
-        component="footer"
-        square
-        variant="outlined"
-      >
-        <Container maxWidth="lg">
-          <Box
-            sx={{
-              flexGrow: 1,
-              justifyContent: "center",
-              display: "flex",
-              my: 1,
-            }}
-          >
-            <div>
-              <img
-                src="/logo-picomystery.png"
-                width={75}
-                height={30}
-                alt="Logo"
-              />
-            </div>
-          </Box>
-
-          <Box
-            sx={{
-              flexGrow: 1,
-              justifyContent: "center",
-              display: "flex",
-              mb: 2,
-            }}
-          >
-            <Typography variant="caption" color="initial">
-              Copyright ©{new Date(Date.now()).getFullYear()}. [] Limited
-            </Typography>
-          </Box>
-        </Container>
-      </Paper> */}
       <div
         className={` p-6 text-center ${
           theme === "light"
