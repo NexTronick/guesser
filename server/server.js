cc49efa995c5f9973b9d97ab03bb374bf8c4cca3
Natraj Padwani <nextronickk@gmail.com>
2024-04-22 04:32:35 +0800

    Removed the static server path.

--- server/server.js
+++ server/server.js
@@ -5,22 +5,22 @@ const animalAPI = require("./routes/animal");
 const path = require("path");
 const port = 5000;
 const _dirname = path.dirname("");
-const buildpath = path.join(_dirname, "../client/build");
+//const buildpath = path.join(_dirname, "../client/build");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/animal", animalAPI);
-app.use(express.static(buildpath));
+// app.use(express.static(buildpath));
 
-app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
-    function (err) {
-      if (err) res.status(500).send(err);
-    }
-  );
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile(
+//     path.join(__dirname, "../client/build/index.html"),
+//     function (err) {
+//       if (err) res.status(500).send(err);
+//     }
+//   );
+// });
 app.listen(port, () => {
   console.log("Listening on port " + port);
   console.log("view link url: http://localhost:" + port);
