ab8b0d22bfbc6c83de4a671b931a1fee7a89381d
Natraj Padwani <nextronickk@gmail.com>
2024-04-22 23:19:35 +0800

    Updated the CORS

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://guesser.natrajpadwani.site, https://guesser-natrajpadwani.netlify.app"
+    "https://guesser-natrajpadwani.netlify.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
